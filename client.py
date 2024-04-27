7e7c0e9fdec86e00dfda032159c111dee62ec6c6
KingCadia <57073780+KingCadia@users.noreply.github.com>
2024-04-27 17:13:11 -0400

    fixed connection on the client side

--- client.py
+++ client.py
@@ -9,6 +9,7 @@ def main():
     nodeSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # connects to the server
+    nodeSocket.connect((PORT, HOST))
     size = nodeSocket.recv(4)
     size = pickle.loads(size)
 
